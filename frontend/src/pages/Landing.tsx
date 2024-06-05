f937e27894bdf67d64a48f10d32bc9bf07fa53da
shubham-gund <shubhamgund91@gmail.com>
2024-06-05 19:48:23 +0530

    add sparke ui

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -7,11 +7,11 @@ export const Landing = ()=>{
   return (
     <div className="flex flex-col items-center justify-center h-[32rem] sm:h-[40rem]  ">
       <LampDemo/>
-      <div className="flex space-x-6 mt-18 sm:mt-0">
-        <Link to={"/signup"} className="flex items-center justify-center w-24 sm:w-40 h-9 sm:h-10 rounded-full  hover:bg-cyan-500 bg-white border dark:border-white border-transparent text-black text-center text-sm font-semibold">
+      <div className="flex space-x-6 mt-16 sm:mt-0">
+        <Link to={"/signup"} className="flex items-center justify-center w-40 h-10 rounded-full  hover:bg-cyan-500 bg-white border dark:border-white border-transparent text-black text-center text-sm font-semibold">
           Join Now
         </Link>
-        <Link to={"/signin"} className="flex items-center justify-center w-24 sm:w-40 h-9 sm:h-10 rounded-full hover:bg-cyan-500 bg-blue-600 border dark:border-white border-transparent text-black text-center text-sm font-semibold">
+        <Link to={"/signin"} className="flex items-center justify-center w-40 =h-10 rounded-full hover:bg-cyan-500 bg-blue-600 border dark:border-white border-transparent text-black text-center text-sm font-semibold">
           Login
         </Link>
       </div>
