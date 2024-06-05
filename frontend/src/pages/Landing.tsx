2b5c95528ad6812d70ee316a722d06cdc9f1a566
shubham-gund <shubhamgund91@gmail.com>
2024-06-05 19:09:54 +0530

    add responsiveness to landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -5,13 +5,13 @@ import { LampDemo } from "../components/ui/lamp";
 export const Landing = ()=>{
 
   return (
-    <div className="flex flex-col items-center justify-center h-[40rem] ">
+    <div className="flex flex-col items-center justify-center h-[32rem] sm:h-[40rem]  ">
       <LampDemo/>
-      <div className="flex space-x-6">
-        <Link to={"/signup"} className="flex items-center justify-center w-40 h-10 rounded-full  hover:bg-slate-500 bg-slate-400 border dark:border-white border-transparent text-black text-center text-sm font-semibold">
+      <div className="flex space-x-6 mt-36 sm:mt-0">
+        <Link to={"/signup"} className="flex items-center justify-center w-40 h-10 rounded-full  hover:bg-cyan-500 bg-white border dark:border-white border-transparent text-black text-center text-sm font-semibold">
           Join Now
         </Link>
-        <Link to={"/signin"} className="flex items-center justify-center w-40 h-10 rounded-full hover:bg-slate-500 bg-slate-400 border dark:border-white border-transparent text-black text-center text-sm font-semibold">
+        <Link to={"/signin"} className="flex items-center justify-center w-40 h-10 rounded-full hover:bg-cyan-500 bg-blue-600 border dark:border-white border-transparent text-black text-center text-sm font-semibold">
           Login
         </Link>
       </div>
