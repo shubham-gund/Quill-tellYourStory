d8ba52929ef4f51ef1a5723bc2741c9f77f69adb
shubham-gund <shubhamgund91@gmail.com>
2024-06-07 18:34:29 +0530

    style: Adjust login button width

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -10,11 +10,11 @@ export const Landing = ()=>{
       <LampDemo />
       </div>
 
-      <div className="flex justify-center items-center space-x-4 absolute inset-x-0 bottom-16 ">
-        <Link to={"/signup"} className="flex items-center justify-center w-40 h-10 rounded-full  hover:bg-cyan-500 bg-white border dark:border-white border-transparent text-black text-center text-sm font-semibold">
+      <div className="flex justify-center items-center sm:space-x-4 space-x-12 absolute inset-x-0 bottom-16 ">
+        <Link to={"/signup"} className="flex items-center justify-center sm:w-40 w-32 h-10 rounded-full  hover:bg-cyan-500 bg-white border dark:border-white border-transparent text-black text-center text-sm font-semibold">
           Join Now
         </Link>
-        <Link to={"/signin"} className="flex items-center justify-center w-40 h-10 rounded-full hover:bg-cyan-500 bg-blue-600 border dark:border-white border-transparent text-black text-center text-sm font-semibold">
+        <Link to={"/signin"} className="flex items-center justify-center sm:w-40 w-32 h-10 rounded-full hover:bg-cyan-500 bg-blue-600 border dark:border-white border-transparent text-black text-center text-sm font-semibold">
           Login
         </Link>
       </div>
